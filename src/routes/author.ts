c342af709a1cbe55b02bc996198ba62c616afdee
Kgwale-NN <kgoalemothiba8@gmail.com>
2026-09-21 21:36:48 +0200

    Final Basic  Routes and CRUD Operations Settings

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -1,39 +1,106 @@
-import { Router,Request,Response } from "express";
-import {body,param,validationResult} from "express-validator"
+import { Router, Request, Response } from "express";
+import { body, param, validationResult } from "express-validator"
 
 const routor = Router()
 
 let authors = [
 
-    {id:1 ,name:"Professor Snape" , email: "Snape@hogwarts.com"},
-    {id:2,name:"Professor Dumbledore" , email: "albus@hogwarts.com"}
+    { id: 1, name: "Professor Snape", email: "Snape@hogwarts.com" },
+    { id: 2, name: "Professor Dumbledore", email: "albus@hogwarts.com" }
 
 ]
 
-routor.get("/" , (req:Request,res:Response) => {
+routor.get("/", (req: Request, res: Response) => {
 
     res.status(200).json(authors)
 
 })
 
-routor.get("/id" ,[param("id").isInt().withMessage("Id must be an interger")] ,(req:Request,res:Response) =>{
+routor.get("/:id", [param("id").isInt().withMessage("Id must be an interger")], (req: Request, res: Response) => {
 
     const errors = validationResult(req)
 
-    console.log(errors,"erros from express-validator middleware")
+    console.log(errors, "erros from express-validator middleware")
 
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
 
-        return res.status(400).json({errors: errors.array()})
+        return res.status(400).json({ errors: errors.array() })
     }
 
-    const {id} = req.params
+    const { id } = req.params
     const author = authors.find((author) => author.id === parseInt(String(id), 10))
 
-    if(!author){
+    if (!author) {
 
-        return res.status(400).send("User not found")
+        return res.status(404).json({ message: "Author not found" })
     }
 
     res.status(200).json(author)
 })
+
+routor.post("/", [body("name").isString().withMessage("Name must be a string"), body("email").isEmail().withMessage("Email must be a valid email address")], (req: Request, res: Response) => {
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    console.log(req)
+
+    const { name, email } = req.body
+    const newAuthor = { id: authors.length + 1, name, email }
+    authors.push(newAuthor)
+
+    res.status(201).json(newAuthor)
+})
+
+routor.put("/:id", [param("id").isInt().withMessage("Id must be an interger"), body("name").isString().withMessage("Name must be a string"), body("email").isEmail().withMessage("Email must be a valid email address")], (req: Request, res: Response) => {
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { id } = req.params
+    const { name, email } = req.body
+
+    const authorIndex = authors.findIndex((author) => author.id === parseInt(String(id), 10))
+
+    if (authorIndex === -1) {
+
+        return res.status(404).json({ message: "Author not found" })
+    }
+
+    authors[authorIndex] = { id: parseInt(String(id), 10), name, email }
+
+    res.status(200).json(authors[authorIndex])
+})
+
+routor.delete("/:id", [param("id").isInt().withMessage("Id must be an interger")], (req: Request, res: Response) => {
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { id } = req.params
+
+    const authorIndex = authors.findIndex((author) => author.id === parseInt(String(id), 10))
+
+    if (authorIndex === -1) {
+
+        return res.status(404).json({ message: "Author not found" })
+    }
+
+    authors.splice(authorIndex, 1)
+
+    res.status(204).send()
+})
+
+export default routor
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import express , {Express} from 'express';
 import { loggerMiddleware } from './middleware/logger';
+import routor from './routes/author';
 
 const app: Express = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 app.use(loggerMiddleware)
+
+app.use("/v1/author",routor);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
