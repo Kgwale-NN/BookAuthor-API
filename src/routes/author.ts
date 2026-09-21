b3694731636c3b9dc3c762235f7d98756b83a964
Kgwale-NN <kgoalemothiba8@gmail.com>
2026-09-21 21:52:30 +0200

    Refactor author routes to use controller functions

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -1,18 +1,12 @@
 import { Router, Request, Response } from "express";
 import { body, param, validationResult } from "express-validator"
+import { addAuthor, getAuthors, getAuthorById, updateAuthor, deleteAuthor } from '../controllers/author'
 
 const routor = Router()
 
-let authors = [
-
-    { id: 1, name: "Professor Snape", email: "Snape@hogwarts.com" },
-    { id: 2, name: "Professor Dumbledore", email: "albus@hogwarts.com" }
-
-]
-
 routor.get("/", (req: Request, res: Response) => {
 
-    res.status(200).json(authors)
+    res.status(200).json(getAuthors())
 
 })
 
@@ -28,7 +22,7 @@ routor.get("/:id", [param("id").isInt().withMessage("Id must be an interger")], 
     }
 
     const { id } = req.params
-    const author = authors.find((author) => author.id === parseInt(String(id), 10))
+    const author = getAuthorById(parseInt(String(id), 10))
 
     if (!author) {
 
@@ -50,8 +44,7 @@ routor.post("/", [body("name").isString().withMessage("Name must be a string"), 
     console.log(req)
 
     const { name, email } = req.body
-    const newAuthor = { id: authors.length + 1, name, email }
-    authors.push(newAuthor)
+    const newAuthor = addAuthor(name, email)
 
     res.status(201).json(newAuthor)
 })
@@ -68,16 +61,14 @@ routor.put("/:id", [param("id").isInt().withMessage("Id must be an interger"), b
     const { id } = req.params
     const { name, email } = req.body
 
-    const authorIndex = authors.findIndex((author) => author.id === parseInt(String(id), 10))
+    const updatedAuthor = updateAuthor(parseInt(String(id), 10), name, email)
 
-    if (authorIndex === -1) {
+    if (!updatedAuthor) {
 
         return res.status(404).json({ message: "Author not found" })
     }
 
-    authors[authorIndex] = { id: parseInt(String(id), 10), name, email }
-
-    res.status(200).json(authors[authorIndex])
+    res.status(200).json(updatedAuthor)
 })
 
 routor.delete("/:id", [param("id").isInt().withMessage("Id must be an interger")], (req: Request, res: Response) => {
@@ -91,14 +82,12 @@ routor.delete("/:id", [param("id").isInt().withMessage("Id must be an interger")
 
     const { id } = req.params
 
-    const authorIndex = authors.findIndex((author) => author.id === parseInt(String(id), 10))
+    const deleted = deleteAuthor(parseInt(String(id), 10))
 
-    if (authorIndex === -1) {
+    if (!deleted) {
 
         return res.status(404).json({ message: "Author not found" })
     }
-
-    authors.splice(authorIndex, 1)
 
     res.status(204).send()
 })
