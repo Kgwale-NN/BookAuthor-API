bca79714536b8985b63a72736b905c9b570a3e88
Kgwale-NN <kgoalemothiba8@gmail.com>
2026-09-21 22:12:33 +0200

    Connect book routes to server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express , {Express} from 'express';
 import { loggerMiddleware } from './middleware/logger';
 import authorRouter from './routes/author';
+import bookRouter from './routes/book';
+
 const app: Express = express();
 
 const PORT = process.env.PORT || 3000;
@@ -10,6 +12,7 @@ app.use(express.json());
 app.use(loggerMiddleware)
 
 app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
