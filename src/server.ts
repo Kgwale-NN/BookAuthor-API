9a09a02750ee13e4b42b9a12cfc7470501d11b36
Kgwale-NN <kgoalemothiba8@gmail.com>
2026-09-21 21:55:13 +0200

    Update server route from /v1/author to /authors

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,15 @@
-import bodyParser from 'body-parser';
 import express , {Express} from 'express';
 import { loggerMiddleware } from './middleware/logger';
-import routor from './routes/author';
-
+import authorRouter from './routes/author';
 const app: Express = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(bodyParser.json());
 
 app.use(loggerMiddleware)
 
-app.use("/v1/author",routor);
+app.use("/authors", authorRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
